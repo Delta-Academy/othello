a4a0d39fb1f1c32049b6013ded5e4cec3943e4c0
neuromantic99 <jrowlandm@gmail.com>
2022-07-21 18:14:18 +0100

    mostly copy and pasted from delta-live-tournaments. Also filled out check_submission

--- main.py
+++ main.py
@@ -1,29 +1,53 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional, Tuple
+import numpy as np
+import torch
+from torch import nn
 
 from check_submission import check_submission
-from game_mechanics import Env, choose_move_randomly, load_pkl, play_game, save_pkl
+from game_mechanics import (
+    OthelloEnv,
+    choose_move_randomly,
+    load_network,
+    play_othello_game,
+    save_network,
+)
 
 TEAM_NAME = "Team Name"  # <---- Enter your team name here!
 assert TEAM_NAME != "Team Name", "Please change your TEAM_NAME!"
 
 
-def train() -> Dict:
+def train() -> nn.Module:
     """
     TODO: Write this function to train your algorithm.
 
     Returns:
+        A pytorch network to be used by choose_move. You can architect
+        this however you like but your choose_move function must be able
+        to use it.
     """
     raise NotImplementedError("You need to implement this function!")
 
 
-def choose_move(state: Any, user_file: Any, verbose: bool = False) -> int:
+def choose_move(
+    state: np.ndarray, neural_network: nn.Module, verbose: bool = False
+) -> Optional[Tuple[int, int]]:
     """Called during competitive play. It acts greedily given current state of the board and value
     function dictionary. It returns a single move to play.
 
     Args:
-        state:
+        state: State of the board as a np array. Your pieces are
+                1's, the opponent's are -1's and empty are 0's.
+        network: The pytorch network output by train().
+        verbose: Whether to print debugging information to console.
 
     Returns:
+        position (Tuple | None): The position (row, col) want to place
+                        your counter. row and col should be an integer 0 -> 7)
+                        where (0,0) is the top left corner and
+                        (7,7) is the bottom right corner. You should return
+                        None if not legal move is available
+
+    n.b. The None return is not ideal maybe think about how to improve this.
     """
     raise NotImplementedError("You need to implement this function!")
 
@@ -31,16 +55,16 @@ def choose_move(state: Any, user_file: Any, verbose: bool = False) -> int:
 if __name__ == "__main__":
 
     ## Example workflow, feel free to edit this! ###
-    file = train()
-    save_pkl(file, TEAM_NAME)
+    my_network = train()
+    save_network(my_network, TEAM_NAME)
 
     check_submission(
         TEAM_NAME
     )  # <---- Make sure I pass! Or your solution will not work in the tournament!!
 
-    my_value_fn = load_pkl(TEAM_NAME)
+    my_network = load_network(TEAM_NAME)
 
-    # Code below plays a single game of Connect 4 against a random
+    # Code below plays a single game of othello against a random
     #  opponent, think about how you might want to adapt this to
     #  test the performance of your algorithm.
     def choose_move_no_value_fn(state: Any) -> int:
@@ -49,12 +73,12 @@ if __name__ == "__main__":
 
         This converts choose_move() to that format.
         """
-        return choose_move(state, my_value_fn)
+        return choose_move(state, my_network)
 
-    play_game(
+    play_othello_game(
         your_choose_move=choose_move_no_value_fn,
         opponent_choose_move=choose_move_randomly,
         game_speed_multiplier=1,
         render=True,
-        verbose=False,
+        verbose=True,
     )
