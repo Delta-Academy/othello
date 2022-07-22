e34a51f3ce51ba1d550a11f45e92828335784d69
neuromantic99 <jrowlandm@gmail.com>
2022-07-22 12:21:00 +0100

    Corrected expected_choose_move_return_type. In check submission, should have been an optional tuple

--- check_submission.py
+++ check_submission.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import torch
+from typing import Optional, Tuple
 from torch import nn
 
 import delta_utils.check_submission as checker
@@ -8,7 +8,7 @@ from game_mechanics import get_empty_board, load_network
 
 def check_submission(team_name: str) -> None:
     example_state = get_empty_board()
-    expected_choose_move_return_type = int
+    expected_choose_move_return_type = Optional[Tuple]
     game_mechanics_expected_hash = (
         "bee9b2a3bec41fb533a51d3b4acca623e2d2cc88cfd309a2edf214330192ea5f"
     )
