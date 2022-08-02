4a26aa79b9573cc43c99167693080f8285cfcd95
neuromantic99 <jrowlandm@gmail.com>
2022-08-02 17:05:28 +0100

    description and author info -> setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,10 @@
 from setuptools import find_packages, setup
 
-setup(name="delta_othello", version="1.0", packages=["delta_othello"])
+setup(
+    name="delta_othello",
+    version="1.0",
+    packages=["delta_othello"],
+    description="Othello with an openAI gym style interface",
+    author="neuromantic99",
+    author_email="james@learney.me",
+)
