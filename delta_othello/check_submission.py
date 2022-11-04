5c01bbc7b6299a21e437e92ffcab53b88aa0d7c9
Henry Pulver <henrypulver@hotmail.co.uk>
2022-11-04 16:43:18 +0000

    Add hash game_mechanics pre-commit hook

--- delta_othello/check_submission.py
+++ delta_othello/check_submission.py
@@ -2,17 +2,13 @@ from pathlib import Path
 from typing import Optional, Tuple
 
 import delta_utils.check_submission as checker
-from torch import nn
-
 from game_mechanics import get_empty_board, load_network
+from torch import nn
 
 
 def check_submission(team_name: str) -> None:
     example_state = get_empty_board()
     expected_choose_move_return_type = tuple
-    game_mechanics_expected_hash = (
-        "91f3f4308395f327e3e578d673e931feaaec9bf33ff4ead7b274ce8a2b701640"
-    )
     expected_pkl_output_type = nn.Module
 
     return checker.check_submission(
@@ -21,6 +17,5 @@ def check_submission(team_name: str) -> None:
         expected_pkl_type=expected_pkl_output_type,
         pkl_file=load_network(team_name),
         pkl_checker_function=lambda x: x,
-        game_mechanics_hash=game_mechanics_expected_hash,
         current_folder=Path(__file__).parent.resolve(),
     )
