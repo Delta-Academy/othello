c27a8427a8fa9ff3b165fcdacd23fdd593976968
neuromantic99 <jrowlandm@gmail.com>
2022-12-12 15:50:00 +0100

    check submission up to date with version 0.1.0

--- delta_othello/check_submission.py
+++ delta_othello/check_submission.py
@@ -1,21 +1,27 @@
+import itertools
 from pathlib import Path
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
-import delta_utils.check_submission as checker
+from delta_utils import get_discrete_choose_move_out_checker
+from delta_utils.check_submission import check_submission as _check_submission
+
 from game_mechanics import get_empty_board, load_network
-from torch import nn
 
 
 def check_submission(team_name: str) -> None:
     example_state = get_empty_board()
-    expected_choose_move_return_type = tuple
-    expected_pkl_output_type = nn.Module
+    user_pkl_file = load_network(team_name)
 
-    return checker.check_submission(
-        example_state=example_state,
-        expected_choose_move_return_type=expected_choose_move_return_type,
-        expected_pkl_type=expected_pkl_output_type,
-        pkl_file=load_network(team_name),
-        pkl_checker_function=lambda x: x,
+    board_dim = example_state.shape[0]
+    possible_outputs: List[Optional[Tuple[int, int]]] = list(
+        itertools.product(range(board_dim), range(board_dim))
+    )
+    possible_outputs.append(None)
+
+    _check_submission(
+        example_choose_move_input={"state": example_state, "neural_network": user_pkl_file},
+        check_choose_move_output=get_discrete_choose_move_out_checker(
+            possible_outputs=possible_outputs,
+        ),
         current_folder=Path(__file__).parent.resolve(),
     )
